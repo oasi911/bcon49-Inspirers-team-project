cdedce9b9b912024f712f43c32075d8737310291
Dan Dudaiev <roffrooff@gmail.com>
2023-02-22 14:34:36 +0200

    Final fix

--- src/js/modal-received.js
+++ src/js/modal-received.js
@@ -2,18 +2,32 @@
   const refs = {
     openModalBtnRec: document.querySelector('[data-modal-open-received]'),
     closeModalBtnRec: document.querySelector('[data-modal-close-received]'),
-    closeOpenModalBtnRec: document.querySelector('[data-modal-close-open-received]'),
+    closeOpenModalBtnRec: document.querySelector(
+      '[data-modal-close-open-received]'
+    ),
     modalRec: document.querySelector('[data-modal-received]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtnRec.addEventListener('click', toggleModalRec);
   refs.closeModalBtnRec.addEventListener('click', toggleModalRec);
   refs.closeOpenModalBtnRec.addEventListener('click', toggleModalRec);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModalRec(event) {
     event.preventDefault();
-        refs.modalRec.classList.toggle('is-hidden');
-    
+    refs.modalRec.classList.toggle('is-hidden');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  }
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
